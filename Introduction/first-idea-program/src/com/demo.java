1de823bc300c9e61996f83dd833f232f4a5fed09
Ubaid <mrubaid56@gmail.com>
2023-07-12 11:48:20 +0530

    Introduction, inputs

---
+++ Introduction/first-idea-program/src/com/demo.java
@@ -0,0 +1,11 @@
+package com;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.print("12345");
+        System.out.println("Hello world");
+        System.out.print("1234594");
+        System.out.print(2232);
+
+    }
+}
