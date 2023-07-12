1b8ca2750865340150db4c32ef217b5624d75811
Ubaid <mrubaid56@gmail.com>
2023-07-12 13:33:43 +0530

    Primitive data types and input/output

---
+++ Introduction/primitives/inputs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter your roll no: ");
+//        int roll = input.nextInt();
+//        System.out.println("Your roll no is :" + roll);
+
+//        int a = 234_000_000; // (_) used instead of (,)
+//        System.out.println(a);
+
+//      String name = input.next();
+//      String name = input.nextLine();
+//       System.out.println(name);
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
+
+    }
+}
---
+++ Introduction/primitives/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        int roll = 65;
+        char letter = 'r';
+        float marks = 98.47f;
+        double largeDecimalNumbers = 244453.54555;
+        long largeInteger = 34444444455453455L;
+        boolean check = false; //true
+
+
+    }
+}
