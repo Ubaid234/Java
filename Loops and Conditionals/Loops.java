30eb2a89418776617522d379c9b845de9d135ad4
Ubaid <mrubaid56@gmail.com>
2023-07-20 13:10:35 +0530

    for loops

---
+++ Loops and Conditionals/Loops.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        // Printing numbers from 1 to 5
+//        for (int num = 1; num <= 5; num++) {
+//            System.out.println(num);
+//        }
+
+        // Print something upto n
+        Scanner input = new Scanner(System.in);
+        int n= input.nextInt();
+
+        for (int num = 1; num <= n; num++) {
+//            System.out.print(num + " ");
+            System.out.println("Hello world");
+        }
+
+    }
+}
