4bfa37d9a1a4377894d8adcc2a3659d107244daf
Ubaid <mrubaid56@gmail.com>
2023-07-20 16:43:02 +0530

    Using Math.max();

--- Loops and Conditionals/Largest.java
+++ Loops and Conditionals/Largest.java
@@ -4,20 +4,27 @@ public class Largest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int c = input.nextInt();
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int c = input.nextInt();
+//
+//        // Find the largest number
+//
+//        int max = a;
+//        if(b > max) {
+//            max = b;
+//        } else if (c > max) {
+//            max = c;
+//        }
+//
+//        System.out.println(max);
 
-        // Find the largest number
+        // Using Math.max
+        int a = 43;
+        int b = 94;
 
-        int max = a;
-        if(b > max) {
-            max = b;
-        } else if (c > max) {
-            max = c;
-        }
+        int maxi = Math.max(a,b);
 
-        System.out.println(max);
-        
+        System.out.println(maxi);
     }
 }
