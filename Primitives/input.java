07dfa48f32c269429f1c83b53fe6c5c769242dcb
Obaid <mrubaid56@gmail.com>
2025-07-12 10:43:09 +0530

    using int with the  input

---
+++ Primitives/input.java
@@ -0,0 +1,12 @@
+package Primitives;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int rollno = input.nextInt();
+        System.out.println("Your rollno is " + rollno);
+    }
+}
