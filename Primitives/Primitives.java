2b64f78dee6b107b83d5940f4ab7dd507e1f0179
Obaid <mrubaid56@gmail.com>
2025-07-12 10:35:02 +0530

    Primitives in java

---
+++ Primitives/Primitives.java
@@ -0,0 +1,18 @@
+// Primitives in Java
+package Primitives;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+      // Primitives
+      
+      int rollno = 64;
+      char letter = 'f';
+      float marks = 98.56f;
+      double largeDecimalNumbers = 65456.465464;
+      long largeInteger = 54645645646445L;
+      boolean check = false;
+
+      
+    }
+}
