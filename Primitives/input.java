efd0a50bba36dfbe4bdea0087ac106d4c6e55c7f
Obaid <mrubaid56@gmail.com>
2025-07-12 10:58:57 +0530

    using primitives and working with input

--- Primitives/input.java
+++ Primitives/input.java
@@ -2,11 +2,44 @@ package Primitives;
 
 import java.util.Scanner;
 
+import javax.print.DocFlavor.STRING;
+
 public class input {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int rollno = input.nextInt();
-        System.out.println("Your rollno is " + rollno);
+        
+    // using int and scanner class
+
+    //     System.out.println("Enter the number");
+    //     int rollno = input.nextInt();
+    //     System.out.println("Your rollno is " + rollno);
+
+    // Using String 
+
+        // String name = input.next();
+        // String name = input.nextLine();
+        // System.out.println(name);
+
+    // Using Float
+
+    // float num = input.nextFloat();
+    // System.out.println(num);
+
+    // using Double
+
+    // double num = input.nextDouble();
+    // System.out.println(num);
+
+    // using char
+
+    // char word = input.next().charAt(0);
+    // System.out.println(word);
+
+    // using boolean
+
+    boolean is = input.nextBoolean();
+    System.out.println(is);
+        
     }
 }
