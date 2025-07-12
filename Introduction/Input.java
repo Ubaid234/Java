5c2ddf276dd587d12d1154684deb61ce988eac54
Obaid <mrubaid56@gmail.com>
2025-07-12 10:19:08 +0530

    Inputs in Java

---
+++ Introduction/Input.java
@@ -0,0 +1,11 @@
+// java program to take an input 
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println(" HEY, NAME YOURSELF");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
