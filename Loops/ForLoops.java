402537ac82c811279d6096913a7701e1449fc32b
Obaid <mrubaid56@gmail.com>
2025-08-02 16:23:46 +0530

    Loops in Java

---
+++ Loops/ForLoops.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        
+        // Print 1-5 using for loops
+        // for (int num =1; num<=5; num += 1) {
+        //     System.out.println(num);
+        // }
+
+        // Print one to n number 
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int num = 1; num <= n ; num++) {
+            // System.out.println(num);
+            // System.out.print(num + " ");
+        // PRINT HELLO WORLD N TIMES
+        System.out.println("Hello world");
+        }
+    }
+}
---
+++ Loops/WhileLoops.java
@@ -0,0 +1,30 @@
+package Loops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        // PRINT 1 TO 10 USING WHLIE LOOPS
+        // int i = 1;
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // DO WHILE LOOP
+        // number 1 to 5
+        // int n = 1;
+        // do {
+        //     System.out.println(n);
+        //     n++;
+        // } while (n <= 5);
+
+        /*
+         * so the do while loop will run once even if the codition is false, eg is given blow 
+         */
+
+         int n = 1;
+         do {
+            System.out.println("Hello world");
+         } while (n != 1);
+    }
+}
