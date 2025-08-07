daa11b3e6d3899771c8ca9f43dbe6753382ca158
Obaid <mrubaid56@gmail.com>
2025-08-07 17:22:02 +0530

    Enhanced for loop

--- Loops/Main.java
+++ Loops/Main.java
@@ -13,8 +13,17 @@ public class Main {
             "Sadiya"
         };
 
+        System.out.println("For Loop");
+        // CAN MAKE CHANGE WITH THIS LIKE USE METHODS
         for(int i=0; i < names.length; i++ ){
             System.out.println(names[i]);
         }
+
+        System.out.println("Enhanced for loop");
+        // CHANGES WON'T WORK HERE AND WOULDN'T AFFECT THE STRING EITHER 
+        for(String name : names ) {
+            System.out.println(name);
+        }
+
     }
 }
