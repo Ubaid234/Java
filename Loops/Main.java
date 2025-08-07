e649907ac8df11c321f2c17a9c9f4a3827413d55
Obaid <mrubaid56@gmail.com>
2025-08-07 17:09:09 +0530

    Loops and Arrays

---
+++ Loops/Main.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Loops and Arrays
+
+        String[] names = {
+            "khalid",// index 1
+            "sajid",
+            "rahil",
+            "ahmed",
+            "Sadiya"
+        };
+
+        for(int i=0; i < names.length; i++ ){
+            System.out.println(names[i]);
+        }
+    }
+}
