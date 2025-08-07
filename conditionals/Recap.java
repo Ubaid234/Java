4f7885e593673d5b1bf750215e60b357862b0480
Obaid <mrubaid56@gmail.com>
2025-08-07 18:01:10 +0530

    Logical Operators

---
+++ conditionals/Recap.java
@@ -0,0 +1,12 @@
+package conditionals;
+
+public class Recap {
+    public static void main(String[] args){
+        // Logical operators Recap
+
+        System.out.println(true && true);
+        System.out.println(true && false && true && true);
+        System.out.println(true || false);
+        System.out.println(true && (false || false));
+    }
+}
