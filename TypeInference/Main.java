41af50b7747b104b780266e7850123358b8ad94b
Obaid <mrubaid56@gmail.com>
2025-09-25 21:26:20 +0530

    VAR KEYWORD

---
+++ TypeInference/Main.java
@@ -0,0 +1,14 @@
+package TypeInference;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Type Inference with var keyword 
+        var name = "khalid";
+        var names = new String[]{"Raheen", "Sehreen"};
+        var age = 22;
+        var isAdult = false;
+        var balance = 1_000_000.33;
+        
+    }
+}
