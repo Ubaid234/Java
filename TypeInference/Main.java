a26dd8817b189baea576179a5e8d9ef57c74ba1a
Obaid <mrubaid56@gmail.com>
2025-09-25 21:32:11 +0530

    how var keyword is used

--- TypeInference/Main.java
+++ TypeInference/Main.java
@@ -1,6 +1,11 @@
 package TypeInference;
 
 public class Main {
+
+        // var keyword will only work for local variable thus the below code will through error i.e
+
+        // private var brand = "Amigos" [wont work, as it is non local]
+
     public static void main(String[] args) {
 
         // Type Inference with var keyword 
@@ -9,6 +14,9 @@ public class Main {
         var age = 22;
         var isAdult = false;
         var balance = 1_000_000.33;
-        
+
+    }
+    public static void printBrand(String brand) {
+        // here again we can not use var keyword instead of string.
     }
 }
